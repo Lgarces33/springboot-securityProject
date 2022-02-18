40d95264e102ede732ba0a0209b6f0856a147dfa
Lgarces33 <garces.luis1@gmail.com>
2022-02-18 08:06:47 -0300

    Last Changes
    Changes mde to deployment

--- authproject/src/test/java/com/luis/authproject/AuthprojectApplicationTests.java
+++ authproject/src/test/java/com/luis/authproject/AuthprojectApplicationTests.java
@@ -1,13 +1,1 @@
-package com.luis.authproject;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AuthprojectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
